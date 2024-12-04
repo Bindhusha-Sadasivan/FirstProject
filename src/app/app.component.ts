566b9c3eb63248930e7658064eaf8a88d7f8b93a
Bindhusha Sadasivan <bindhushasadasivan@gmail.com>
2024-12-05 05:29:19 +0530

    Creation of a SPA without property binding and bootsrap installation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  servers:any = [];
+serverElements:any = [];
+newServerName:any ='';
+newSeverContent:any = '';
 
-  onAddServer(){
-    this.servers.push("Add another server");
-  }
+onAddNewServer(){
+  this.serverElements.push({
+    type: "addServer",
+    name: this.newServerName,
+    content: this.newSeverContent
+  });
+}
 
-  onRemoveServer(i:any){
-    this.servers.splice(i,1)
-  }
+onAddNewBluePrint(){
+  this.serverElements.push({
+    type: "bluePrintServer",
+    name: this.newServerName,
+    content: this.newSeverContent
+
+  })
+}
 
 }
